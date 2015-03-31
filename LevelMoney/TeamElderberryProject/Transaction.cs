2cfd0117039bff5bf4e3e732bd3cfb5dac77b83d
DimitarSD <dsd363978@yahoo.com>
2015-03-31 14:47:22 +0300

    Start project

---
+++ LevelMoney/TeamElderberryProject/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamElderberryProject
+{
+    public abstract class Transaction
+    {
+        // struct propertie
+    }
+}
