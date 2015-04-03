518aa972e25911cd6aae8c3b997f9be39fef72b9
zdzdz <zdzdz.link@yahoo.com>
2015-04-03 03:03:36 +0300

    Exception class

---
+++ LevelMoney/TeamElderberryProject/InputException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamElderberryProject
+{
+    [Serializable]
+    public class InputException : Exception
+    {
+        public InputException()
+            : base()
+        {
+        }
+
+
+        public InputException(string message)
+            : base(message)
+        {
+        }
+
+        public InputException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public InputException(SerializationInfo info, StreamingContext context) 
+            : base(info, context)
+        {
+        }
+    }
+}
